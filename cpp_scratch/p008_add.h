2baec4cce674489bdeebab3b2cf57f81c05ca472
Anirudh Swaminathan <aniswami97@gmail.com>
2018-05-05 23:45:01 +0530

    Completed 1.9. Resolved linker errors for 1.8
    - Resolved the linker error for p008_main.cpp and p008_add.cpp by doing
      the following :-
    
        Got both files to work together without a .h file
        by compiling manually instead of compiling on CodeBlocks.
    
    - Also used .h files to complete the linking tutorial given in 1.9

--- cpp_scratch/p008_add.h
+++ cpp_scratch/p008_add.h
@@ -1,6 +1,11 @@
+// Start of the header guard
+// _ADD_H can be any unique name
+// We give it the same name as that of the file name
 #ifndef _ADD_H_
 #define _ADD_H_
 
+// Function prototype for add()
 int add(int, int);
 
+// end of the header guard
 #endif // _ADD_H_
--- cpp_scratch/p008_main.cpp
+++ cpp_scratch/p008_main.cpp
@@ -3,7 +3,11 @@
 // The following line works only if the program is in the same project
 // int add(int x,  int y);
 
-// The following is a try of my hack for making it work
+// This is a forward declaration of add() that is linked at run-time
+// when compiled along with p008_add.cpp
+// int add(int, int);
+
+// The following line includes a user-defined header file
 #include "p008_add.h"
 
 int main()

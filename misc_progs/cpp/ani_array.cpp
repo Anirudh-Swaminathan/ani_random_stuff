f6b3966a2e21d4bcda12ef28f6ccfdbe05cfdaec
Anirudh Swaminathan <aniswami97@gmail.com>
2022-07-22 10:16:19 -0700

    C++ std::array explore
    Next up: span, vector, array difference
    decltype, shared_ptr<>, base class pointer for derived classes
    std::decay_t; noexcept; C++ lambdas; templates;
    Constructor for object members, emplace_back;
    std::set<>; std::less<>; partial template specialization; RAII;
    priority_queue<> comparator; C++ enum; operator overloading
    constexpr, const vector<const void*>& vec; implicit downcasting

---
+++ misc_progs/cpp/ani_array.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    for (const auto count : std::array{ 4u })
+    {
+        cout << "count is " << count << endl;
+    }
+    return 0;
+}

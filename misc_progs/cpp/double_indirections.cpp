63c3a98848c20af2301320829d7e99a65d784e4d
Anirudh Swaminathan <aniswami97@gmail.com>
2024-03-01 11:00:21 -0800

    C++ experiment with function calls
    Experiment with pass by value, pass by reference, pass by pointer value,
    pass by pointer reference and pass by double pointer value

---
+++ misc_progs/cpp/double_indirections.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+using namespace std;
+
+void func(int a)
+{
+    cout << "In func pass by value, a = " << a << endl;
+    cout << "In func pass by value, &a = " << &a << endl;
+    a = 50;
+    cout << "In func pass by value, a = " << a << endl;
+    cout << "In func pass by value, &a = " << &a << endl;
+    return;
+}
+
+void funcref(int& a)
+{
+    cout << "In func pass by reference, a = " << a << endl;
+    cout << "In func pass by reference, &a = " << &a << endl;
+    a = 75;
+    cout << "In func pass by reference, a = " << a << endl;
+    cout << "In func pass by reference, &a = " << &a << endl;
+    return;
+}
+
+void func(int *a)
+{
+    cout << "In func pass by pointer value, *a = " << *a << endl;
+    cout << "In func pass by pointer value, a = " << a << endl;
+    cout << "In func pass by pointer value, &a = " << &a << endl;
+    *a = 100;
+    cout << "In func pass by pointer value, *a = " << *a << endl;
+    cout << "In func pass by pointer value, a = " << a << endl;
+    cout << "In func pass by pointer value, &a = " << &a << endl;
+}
+
+void funcref(int *& a)
+{
+    cout << "In func pass by pointer reference, *a = " << *a << endl;
+    cout << "In func pass by pointer reference, a = " << a << endl;
+    cout << "In func pass by pointer reference, &a = " << &a << endl;
+    *a = 150;
+    cout << "In func pass by pointer reference, *a = " << *a << endl;
+    cout << "In func pass by pointer reference, a = " << a << endl;
+    cout << "In func pass by pointer reference, &a = " << &a << endl;
+}
+
+void func(int **a)
+{
+    cout << "In func pass by double pointer, **a = " << **a << endl;
+    cout << "In func pass by double pointer, *a = " << *a << endl;
+    cout << "In func pass by double pointer, a = " << a << endl;
+    cout << "In func pass by double pointer, &a = " << &a << endl;
+    **a = 125;
+    cout << "In func pass by double pointer, **a = " << **a << endl;
+    cout << "In func pass by double pointer, *a = " << *a << endl;
+    cout << "In func pass by double pointer, a = " << a << endl;
+    cout << "In func pass by double pointer, &a = " << &a << endl;
+}
+
+int main()
+{
+    int a = 25;
+    int* ptr = &a;
+    cout << "main initializes a = " << a << endl;
+    cout << "In main, memory address of a = " << &a << endl;
+    cout << "In main, ptr = " << ptr << endl;
+    cout << "In main, memory address of ptr = " << &ptr << endl;
+    func(a);
+    cout << "In main, a = " << a << endl;
+    cout << "In main, memory address of a = " << &a << endl;
+    cout << "In main, ptr = " << ptr << endl;
+    cout << "In main, memory address of ptr = " << &ptr << endl;
+    funcref(a);
+    cout << "In main, a = " << a << endl;
+    cout << "In main, memory address of a = " << &a << endl;
+    cout << "In main, ptr = " << ptr << endl;
+    cout << "In main, memory address of ptr = " << &ptr << endl;
+    func(ptr);
+    cout << "In main, a = " << a << endl;
+    cout << "In main, memory address of a = " << &a << endl;
+    cout << "In main, ptr = " << ptr << endl;
+    cout << "In main, memory address of ptr = " << &ptr << endl;
+    funcref(ptr);
+    cout << "In main, a = " << a << endl;
+    cout << "In main, memory address of a = " << &a << endl;
+    cout << "In main, ptr = " << ptr << endl;
+    cout << "In main, memory address of ptr = " << &ptr << endl;
+    func(&ptr);
+    cout << "In main, a = " << a << endl;
+    cout << "In main, memory address of a = " << &a << endl;
+    cout << "In main, ptr = " << ptr << endl;
+    cout << "In main, memory address of ptr = " << &ptr << endl;
+    return 0;
+}

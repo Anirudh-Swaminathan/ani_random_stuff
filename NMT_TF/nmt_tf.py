ac0ab7ed2f1c3d6eb8bd05535985041228f2f414
Anirudh Swaminathan <aniswami97@gmail.com>
2024-12-17 17:32:28 -0800

    Tokenize and detokenize example sentences
    Distribution of tokens in dataset is plotted on histogram as well for
    reference

--- NMT_TF/nmt_tf.py
+++ NMT_TF/nmt_tf.py
@@ -40,5 +40,51 @@ tf.keras.utils.get_file(
         f"https://storage.googleapis.com/download.tensorflow.org/models/{model_name}.zip",
         cache_dir='.', cache_subdir='', extract=True
     )
-tokenizers = tf.saved_model.load(model_name)
+tokenizers = tf.saved_model.load(f"{model_name}_extracted/{model_name}")
+
+# print tokenized sentences
+print(f"> This is a batch of strings:")
+for en in en_examples.numpy():
+    print(en.decode('utf-8'))
+
+# tokenize the examples
+print(f"Tokenizing the examples")
+encoded = tokenizers.en.tokenize(en_examples)
+print(f"> This is a padded-batch of token IDs:")
+for row in encoded.to_list():
+    print(row)
+
+# detokenize the tokenized examples
+print(f"Detokenizing the tokenized examples now")
+round_trip = tokenizers.en.detokenize(encoded)
+print(f"> This is human-readable text:")
+for line in round_trip.numpy():
+    print(line.decode('utf-8'))
+
+# lower level lookup method
+print(f"Lower level lookup method converts from token-IDs to token text")
+print(f"> This is the test split into tokens:")
+tokens = tokenizers.en.lookup(encoded)
+print(tokens)
+
+# Distribution of tokens per example in the dataset
+lengths = list()
+for pt_examples, en_examples in train_examples.batch(1024).cache():
+    pt_tokens = tokenizers.pt.tokenize(pt_examples)
+    lengths.append(pt_tokens.row_lengths())
+
+    en_tokens = tokenizers.en.tokenize(en_examples)
+    lengths.append(en_tokens.row_lengths())
+    print('.', end='', flush=True)
+
+all_lengths = np.concatenate(lengths)
+
+# plot a histogram of the tokens per example in the dataset
+plt.hist(all_lengths, np.linspace(0, 500, 101))
+plt.ylim(plt.ylim())
+max_length = max(all_lengths)
+plt.plot([max_length, max_length], plt.ylim())
+plt.title(f"Maximum tokens per example: {max_length}")
+plt.savefig("token_distribution_histogram.png")
+plt.show()
 

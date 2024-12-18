ecb7cf0bc99b43fe558e092fdd3336e98f053bbd
Anirudh Swaminathan <anirudh.swaminathan@intel.com>
2024-12-18 15:18:21 -0800

    Prepare training batch for Translation task
    Teacher Forcing is used which includes the original target translation
    as input to the next timestep, rather than feeding the current timestep
    prediction to the next timestep as input. This is less stable, but more
    efficient since the model does not need to be run sequentially

--- NMT_TF/nmt_tf.py
+++ NMT_TF/nmt_tf.py
@@ -42,7 +42,10 @@ tf.keras.utils.get_file(
     )
 tokenizers = tf.saved_model.load(model_name)
 
-# print tokenized sentences
+tok_li = [item for item in dir(tokenizers.en) if not item.startswith('_')]
+print(f"Tokenizer methods available: {tok_li}")
+
+# print text examples sentences
 print(f"> This is a batch of strings:")
 for en in en_examples.numpy():
     print(en.decode('utf-8'))
@@ -88,3 +91,94 @@ plt.title(f"Maximum tokens per example: {max_length}")
 plt.savefig("token_distribution_histogram.png")
 plt.show()
 
+# Data pipeline with tf.data
+# Tokenizes into ragged batches. Trims to be no longer than MAX_TOKENS. Splits the target tokens into inputs and labels.
+# Shifts labels by 1 step so that at each input location, the label is the ID of the next token.
+# Converts the RaggedTensors to padded dense Tensors.
+# returns an (inputs, labels) pair.
+MAX_TOKENS = 128
+def prepare_batch(pt, en):
+    """
+    Tokenize, trim, and split the examples into an (inputs, labels) pair
+    Converts the RaggedTensors to padded dense Tensors.
+    @Args:
+    pt: Portuguese text
+    en: English text
+
+    @Returns:
+    (pt_tokens, en_inputs), en_labels pair
+    """
+    # Output is ragged.
+    pt_tokens = tokenizers.pt.tokenize(pt)
+
+    # Trim to MAX_TOKENS
+    pt_tokens = pt_tokens[:, :MAX_TOKENS]
+
+    # Convert to 0-padded dense Tensor
+    pt_tokens = pt_tokens.to_tensor()
+
+    # Tokenize the English text
+    en_tokens = tokenizers.en.tokenize(en)
+
+    # Trim to MAX_TOKENS + 1 to allow for shifted by 1 labels
+    en_tokens = en_tokens[:, :(MAX_TOKENS + 1)]
+
+    # Drop the [END] tokens
+    en_inputs = en_tokens[:, :-1].to_tensor()
+
+    # Drop the [START] tokens
+    en_labels = en_tokens[:, 1:].to_tensor()
+
+    return (pt_tokens, en_inputs), en_labels
+
+# Convert dataset of text examples into data of batches for training
+# Tokenize the text. Filter out sequences that are too long. Tokenizer is much more efficient on large batches.
+# Cache the dataset to memory to get a speedup while reading from it.
+# Shuffle and denst_to_ragged_batch randomizes the order and assembles batches of examples.
+# prefetch runs the dataset in parallel with the model to ensure data is available when needed.
+BUFFER_SIZE = 20000
+BATCH_SIZE = 64
+def make_batches(ds):
+    """
+    Convert dataset of text examples into data of batches for training
+    @Args:
+    ds: dataset of text examples
+
+    @Returns:
+    batched
+    """
+    return (
+        ds
+        .cache()
+        .shuffle(BUFFER_SIZE)
+        .batch(BATCH_SIZE)
+        .map(prepare_batch, num_parallel_calls=tf.data.AUTOTUNE)
+        .prefetch(tf.data.AUTOTUNE)
+    )
+
+# Test the Dataset
+# create training and validation set batches
+train_batches = make_batches(train_examples)
+val_batches = make_batches(val_examples)
+
+# Resulting tf.data.Dataset objects are setup for training with Keras.
+# Model.fit training expects (inputs, labels) pairs.
+# inputs are pairs of tokenized Portuguese and English sentences (pt_tokens, en_inputs).
+# labels are the English sentences, offset by 1 token (en_labels).
+# Similar to text generation, except the model has additional input "context" (the Portuguese sequence) that the model is conditioned on
+# Teacher Forcing: regardless of the model's prediction at each timestep, it gets the true value as input for the next timestep.
+# Simple and efficient, because the model doesn't need to run sequentially.
+# Outputs at the different sequence locations are computed in parallel
+# Alternative: Just provide Portuguese text as input and let the model generate the English translation.
+# Alternative is slower since time steps can't run in parallel
+# Alternative is a harder task to learn, since the model can't get the end of a sentence correct until it gets the beginning correct.
+# Alternative can give a more stable model because the model has to learn to correct its own errors during training.
+for (pt, en), en_labels in train_batches.take(1).cache():
+    print(f"Portuguese shape: {pt.shape}")
+    print(f"English shape: {en.shape}")
+    print(f"English label shape: {en_labels.shape}")
+    break
+
+print(f"Checking if shifted labels match the English text")
+print(en[0][:10])
+print(en_labels[0][:10])
